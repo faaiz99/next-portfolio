1ac666573962a5c50d5ad84c1b0e70a95faaf8e6
faaiz99 <44613754+faaiz99@users.noreply.github.com>
2023-08-08 16:53:40 +0500

    remoded getStaticProps

--- app/components/Work.js
+++ app/components/Work.js
@@ -1,6 +1,3 @@
-
-
-
 const Work = ({ repos }) => {
 
     return (
--- app/page.js
+++ app/page.js
@@ -6,13 +6,13 @@ import Work from './components/Work'
 
 
 
-export const getStaticProps = async () => {
-  const token = process.env.GITHUB_TOKEN
-  const name = process.env.GITHUB_USER_NAME
-  const res = await fetch('https://api.github.com/repos/vercel/next.js', { next: { revalidate: 100 } })
-  const repo = await res.json()
-  return { props: { repo } }
-}
+// export const getStaticProps = async () => {
+//   const token = process.env.GITHUB_TOKEN
+//   const name = process.env.GITHUB_USER_NAME
+//   const res = await fetch('https://api.github.com/repos/vercel/next.js', { next: { revalidate: 100 } })
+//   const repo = await res.json()
+//   return { props: { repo } }
+// }
 
 export default function Main({ repo }) {
   return (
