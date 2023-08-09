b07b7975348042ac5580b928812276ced54cd6b8
faaiz99 <44613754+faaiz99@users.noreply.github.com>
2023-08-09 13:02:19 +0500

    Edge Function Setup

---
+++ app/api/test/route.js
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(request) {
+
+    return NextResponse.json(
+      {
+            message: 'hello'
+        },  { status: 200 }
+      
+    )
+  }
