ae6e01ba4faa3267dec0594140cb39720b524a23
faaiz99 <44613754+faaiz99@users.noreply.github.com>
2023-08-18 03:10:27 +0500

    -export

--- next.config.js
+++ next.config.js
@@ -1,20 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   // output: 'export',
+   output: 'export',
    images: { unoptimized: true },
-   async headers() {
-      return [
-        {
-          source: '/api/',
-          headers: [
-            {
-              key: 'X-Content-Type',
-              value: 'application/json',
-            },
-          ],
-        },
-      ]
-    },
+   
 
 }
 
