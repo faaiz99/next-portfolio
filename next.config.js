e406739079006a7e54692214809531cb3ab9f0cc
faaiz99 <44613754+faaiz99@users.noreply.github.com>
2023-08-18 12:21:48 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   output: 'standalone',
+   output: 'export',
    images: { unoptimized: true },
    async headers() {
       return [
