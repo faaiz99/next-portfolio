60e6655bd123a12fd4c5f7eb7ff8f8c0cdef3e1e
faaiz99 <44613754+faaiz99@users.noreply.github.com>
2023-08-18 12:24:59 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,28 +2,6 @@
 const nextConfig = {
    output: 'export',
    images: { unoptimized: true },
-   async headers() {
-      return [
-         {
-            source: '/api/contact',
-            headers: [
-               {
-                  key: "Access-Control-Allow-Methods",
-                  value: "GET,POST"
-               },
-               {
-                  key: "Access-Control-Allow-Credentials",
-                  value: "true"
-               },
-               {
-                  key: 'x-another-custom-header',
-                  value: 'my other custom header value',
-               },
-
-            ]
-         }
-      ]
-   }
 }
 
 module.exports = nextConfig
