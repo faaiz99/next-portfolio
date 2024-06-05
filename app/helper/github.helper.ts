e9a68fd188ddd5f323a01aaeb7fccef3cb11a1ef
faaiz99 <faaizaslam75@live.com>
2024-06-06 00:54:52 +0500

    fix: build error

--- app/helper/github.helper.ts
+++ app/helper/github.helper.ts
@@ -1,94 +1,80 @@
 import { octokit } from "./gh.helper";
 import { OctokitResponse } from "@octokit/types";
 import { supabase } from "./db.helper";
-import User from "../Types/User";
-import Repo from "../Types/Repos";
+import { UserResponse, User } from "../Types/User";
+import {Repo, RepoResponse } from "../Types/Repos";
 
 // User
 
-export const getUserFromGH = async (): Promise<boolean> => {
+export const getUserFromGH = async (): Promise<UserResponse> => {
   try {
-    const user: OctokitResponse<any> = await octokit.request("GET /users/:username", {
+    return await octokit.request("GET /users/:username", {
       username: `${process.env.NEXT_PUBLIC_USER_NAME}`,
       headers: {
         "X-GitHub-Api-Version": "2022-11-28",
         Accept: "application/vnd.github+json",
       },
-    });
-    await transformUser(user);
-    return true;
-  } catch (error) {
-    console.error('Error fetching user from GitHub:', error);
-    return false;
+    }); 
+  } catch (error:any) {
+    throw new Error(`Error fetching user from GitHub: ${error.message}`)
   }
 };
 
-export const transformUser = async (user: OctokitResponse<any>): Promise<void> => {
-  const {
-    id,
-    login,
-    avatar_url,
-    public_repos,
-    followers,
-    following,
-    created_at,
-    updated_at,
-  } = user.data;
-
-  const mappedUser: User = {
-    id,
-    login,
-    avatar_url,
-    public_repos,
-    followers,
-    following,
-    created_at,
-    updated_at,
-  };
-
-  await insertUserToDB(mappedUser);
+export const userMapper = (user: UserResponse): User => {
+  return {
+    id: user.data.id,
+    login: user.data.login,
+    avatar_url: user.data.avatar_url,
+    public_repos: user.data.public_repos,
+    followers: user.data.followers,
+    following: user.data.following,
+    created_at: user.data.created_at,
+    updated_at: user.data.updated_at,
+  } as User;
 };
 
 export const insertUserToDB = async (user: User): Promise<boolean> => {
   const { id, login, public_repos, followers, following, avatar_url } = user;
-  const { error } = await supabase
+  const {  error } = await supabase
     .from("users")
     .update({
-      login: login,
-      public_repos: public_repos,
-      followers: followers,
-      following: following,
-      avatar_url: avatar_url,
+      login,
+      public_repos,
+      followers,
+      following,
+      avatar_url,
       updated_at: new Date().toString(),
     })
     .eq("id", id);
-  if (error) {
-    return false;
-  }
+  if (error) return false;
   return true;
 };
 
 // Repositories
-export const getReposFromGH = async (): Promise<boolean> => {
+export const getReposFromGH = async (): Promise<any> => {
   try {
-    const repos: OctokitResponse<any> = await octokit.request("GET /users/{username}/repos", {
+    return await octokit.request("GET /users/{username}/repos", {
       username: `${process.env.NEXT_PUBLIC_USER_NAME}`,
       headers: {
         "X-GitHub-Api-Version": "2022-11-28",
         Accept: "application/vnd.github+json",
       },
     });
-    await transformRepo(repos);
-    return true;
-  } catch (error) {
-    console.error('Error fetching repositories from GitHub:', error);
-    return false;
+  } catch (error:any) {
+    throw new Error (`Error fetching repositories from GitHub: ${error.message}`)
   }
 };
-export const transformRepo = async (repos: OctokitResponse<any>): Promise<void> => {
-  const mappedRepos: Repo[] = repos.data.map((repo: Repo) => {
-    const {
-      id,
+export const reposMapper = (repos: RepoResponse): Repo[] => {
+  const mappedRepos: Repo[] = repos.data.map(({  
+    id,
+    name,
+    html_url,
+    description,
+    language,
+    created_at,
+    updated_at,
+    forks_count,
+    open_issues_count,}: Repo) => ({  id,
       name,
       html_url,
       description,
@@ -96,27 +82,13 @@ export const transformRepo = async (repos: OctokitResponse<any>): Promise<void> 
       created_at,
       updated_at,
       forks_count,
-      open_issues_count,
-    } = repo;
-
-    return {
-      id,
-      name,
-      html_url,
-      description,
-      language,
-      created_at,
-      updated_at,
-      forks_count,
-      open_issues_count,
-    };
-  });
-
-  await insertReposToDB(mappedRepos);
+      open_issues_count,}));
+  return mappedRepos;
 };
+
 export const insertReposToDB = async (repos: Repo[]): Promise<boolean> => {
   try {
-    const { data, error } = await supabase.from("repos").upsert(repos);
+    const { error } = await supabase.from("repos").upsert(repos);
     if (error) {
       console.error('Error inserting repositories to DB:', error);
       return false;
@@ -128,3 +100,20 @@ export const insertReposToDB = async (repos: Repo[]): Promise<boolean> => {
   }
 };
 
+async function processGHData(): Promise<boolean> {
+
+  try {
+    const user = await getUserFromGH();
+    const transformedUser = userMapper(user)
+    const isUserSuccess = await insertUserToDB(transformedUser);
+
+    const repos = await getReposFromGH();
+    const transformedRepos = reposMapper(repos)
+    const isRepoSuccess = await insertReposToDB(transformedRepos);
+  } catch (error: any) {
+    return false
+  }
+  return true
+}
+
+export { processGHData }
