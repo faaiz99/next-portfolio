42a421cb467322fceefe04c483211cfbcf02ef88
faaiz99 <faaizaslam75@live.com>
2024-06-05 23:15:57 +0500

    feat: vercel analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/Footer";
 import { NextFont } from "next/dist/compiled/@next/font";
+import { Analytics } from '@vercel/analytics/react';
+
 const inter: NextFont = Inter({ subsets: ["latin"] });
 
 interface Metadata {
@@ -27,6 +29,7 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Analytics/>
         <Footer />
       </body>
     </html>
