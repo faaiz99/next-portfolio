208b77f1beb544ac837662c705522a3c51684e3c
faaiz99 <faaizaslam75@live.com>
2024-08-02 23:45:27 +0500

    fix(test): contact test

--- app/tests/components/Contact.test.tsx
+++ app/tests/components/Contact.test.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
 import { it, describe } from "vitest";
-
 import Contact from "../../components/Contact";
+
+vi.mock("@supabase/supabase-js", () => {
+  return {
+    createClient: vi.fn().mockReturnValue({
+      auth: {
+        signIn: vi.fn(),
+        signOut: vi.fn(),
+      },
+      from: vi.fn().mockReturnValue({
+        select: vi.fn().mockResolvedValue({ data: [], error: null }),
+      }),
+    }),
+  };
+});
+
 
 describe("Contact ", () => {
   it("Should Render", () => {
