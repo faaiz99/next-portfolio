6878e47c3865ae886c87685b09031029ff834bb6
faaiz99 <faaizaslam75@live.com>
2024-08-15 18:47:40 +0500

    refactor: remove redundant code

--- next.config.js
+++ next.config.js
@@ -2,36 +2,7 @@
 const nextConfig = {
   crossOrigin: "anonymous",
   images: { unoptimized: true },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, OPTIONS, POST",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-Requested-With, Content-Type, Accept",
-          },
-        ],
-      },
-    ];
-  },
-  experimental: {
-    serverActions: {
-      allowedOrigins: [
-        "https://faaiz-portfolio-pre-prod.vercel.app",
-        "https://faaiz-portfolio-prod.vercel.app",
-      ],
-      bodySizeLimit: "2mb",
-    },
-  },
+  output: "export"
 };
 
 module.exports = nextConfig;
