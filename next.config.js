88b25f9fa9389f913ed034b6a8b3f22182e4826a
faaiz99 <faaizaslam75@live.com>
2024-08-15 18:01:22 +0500

    fix: added server actions cors

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
+  experimental:{
+    serverActions:{
+      allowedOrigins: ["https://faaiz-portfolio-pre-prod.vercel.app","https://faaiz-portfolio-prod.vercel.app"],
+      bodySizeLimit: "2mb",
+    }
+  }
 };
 
 module.exports = nextConfig;
