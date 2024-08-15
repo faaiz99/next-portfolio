b6c1111f7785401b0d0b7ee355ea2b9df59836f4
faaiz99 <faaiz99@users.noreply.github.com>
2024-08-15 13:01:55 +0000

    Apply code quality changes

--- next.config.js
+++ next.config.js
@@ -2,12 +2,15 @@
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
-  experimental:{
-    serverActions:{
-      allowedOrigins: ["https://faaiz-portfolio-pre-prod.vercel.app","https://faaiz-portfolio-prod.vercel.app"],
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "https://faaiz-portfolio-pre-prod.vercel.app",
+        "https://faaiz-portfolio-prod.vercel.app",
+      ],
       bodySizeLimit: "2mb",
-    }
-  }
+    },
+  },
 };
 
 module.exports = nextConfig;
