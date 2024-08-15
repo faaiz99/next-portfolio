a6c3bb8fa937d16604361e664cb27b3580c41cb8
faaiz99 <faaizaslam75@live.com>
2024-08-15 17:08:33 +0500

    debug: route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,20 +2,20 @@ import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../../Types/Supabase";
 
-const url: string = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
-const key: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
-const supabase = createClient<Database>(url, key, {
-  auth: { persistSession: false },
-});
+// const url: string = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
+// const key: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
+// const supabase = createClient<Database>(url, key, {
+//   auth: { persistSession: false },
+// });
 
 export async function POST(req: Request) {
   try {
     const data = await req.json();
     const { name, email, message } = data.contact;
-    const { error } = await supabase
-      .from("messages")
-      .insert([{ name, email, message }]);
-    if (error) throw error;
+    // const { error } = await supabase
+    //   .from("messages")
+    //   .insert([{ name, email, message }]);
+    // if (error) throw error;
     // return NextResponse.redirect(req.url, 302);
     return NextResponse.json(
       { status: true, message: "Success" },
