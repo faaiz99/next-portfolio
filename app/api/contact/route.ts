873a0778b46b0aef04d267395b235ccdfdfb4921
faaiz99 <faaizaslam75@live.com>
2024-08-15 17:31:39 +0500

    fix: req.url transformed

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,26 +1,27 @@
-import { NextResponse } from "next/server";
-// import { createClient } from "@supabase/supabase-js";
+import { NextResponse, NextRequest } from "next/server";
+import { createClient } from "@supabase/supabase-js";
 import { Database } from "../../Types/Supabase";
 
-// const url: string = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
-// const key: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
-// const supabase = createClient<Database>(url, key, {
-//   auth: { persistSession: false },
-// });
+const url: string = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
+const key: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
+const supabase = createClient<Database>(url, key, {
+  auth: { persistSession: false },
+});
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   try {
     const data = await req.json();
     const { name, email, message } = data.contact;
-    // const { error } = await supabase
-    //   .from("messages")
-    //   .insert([{ name, email, message }]);
-    // if (error) throw error;
-    // return NextResponse.redirect(req.url, 302);
-    return NextResponse.json(
-      { status: true, message: "Success" },
-      { status: 201 },
-    );
+    const { error } = await supabase
+      .from("messages")
+      .insert([{ name, email, message }]);
+    if (error) throw error;
+    const url = req.url
+      .replace("/api/contact", "")
+      .replace(/\/$/, "")
+      .replace(/\/$/, "");
+    return NextResponse.redirect(url, 308);
+
   } catch (error) {
     return error instanceof Error
       ? NextResponse.json({ error: error.message }, { status: 500 })
