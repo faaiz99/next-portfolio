30a90062879e63945cfb2b1c0da8f03b359d860d
faaiz99 <faaiz@trilops.com>
2024-09-08 03:43:29 +0500

    fix: build error due to missing theme import

--- app/components/navbar/ThemeSwitch.tsx
+++ app/components/navbar/ThemeSwitch.tsx
@@ -6,19 +6,20 @@ import { useTheme } from "next-themes";
 const ThemeSwitch: React.FC = () => {
   const { theme, setTheme } = useTheme();
 
-  useEffect(() => {
-    const storedTheme = localStorage.getItem("theme");
-    if (storedTheme) {
-      setTheme(storedTheme);
-    } else {
-      setTheme("light");
-    }
-  }, [setTheme]);
+  // useEffect(() => {
+  //   const storedTheme = localStorage.getItem("theme");
+  //   if (storedTheme) {
+  //     setTheme(storedTheme);
+  //   } else {
+  //     setTheme("light");
+  //   }
+  // }, [setTheme]);
 
   const toggleTheme = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
-    setTheme(newTheme);
-    localStorage.setItem("theme", newTheme);
+    window.alert("Not implemented yet");
+    // const newTheme = theme === "light" ? "dark" : "light";
+    // setTheme(newTheme);
+    // localStorage.setItem("theme", newTheme);
   };
 
   return (
