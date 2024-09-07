6c1531faba2d227d79fea3cdf93b990222d6dc47
faaiz99 <faaiz@trilops.com>
2024-09-08 04:14:08 +0500

    fix: added cron routes

---
+++ app/api/github/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { processGHData } from "../../helper/github.helper";
+
+export async function GET() {
+  try {
+    const result = await processGHData();
+    if (!result) {
+      return NextResponse.json(
+        { message: "No data was fetched and updated", operation: false },
+        {
+          status: 204,
+        },
+      );
+    }
+    return NextResponse.json(
+      { message: "User & Repos fetched and updated", operation: result },
+      {
+        status: 200,
+      },
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: "User & Repos could not be fetched and updated",
+        operation: false,
+        error: error.message,
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
