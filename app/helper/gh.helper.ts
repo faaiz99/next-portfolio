be32d81325c5c5e5e14ae63355dff0c86c5cd2b4
faaiz99 <faaiz99@users.noreply.github.com>
2024-09-07 23:14:42 +0000

    eslint-prettier: auto-fixes

--- app/helper/gh.helper.ts
+++ app/helper/gh.helper.ts
@@ -1,4 +1,4 @@
-import {Octokit} from "@octokit/rest"
+import { Octokit } from "@octokit/rest";
 export const octokit = new Octokit({
   auth: `${process.env.NEXT_PUBLIC_TOKEN}`,
   baseUrl: "https://api.github.com",
